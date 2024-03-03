45dd680a467a122af0062fe08fd177d2b9eeafef
Marco <marco1102004@gmail.com>
2024-03-03 12:11:39 -0600

    añadiendo peticion index

--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environments.local';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserInterface } from '../Models/User.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private urlIndex = environment.index
 
-  constructor() { }
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  getUsers():Observable<UserInterface[]>{
+    return this.http.get<UserInterface[]>(this.urlIndex)
+  }
 }
