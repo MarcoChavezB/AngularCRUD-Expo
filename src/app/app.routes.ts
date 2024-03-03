bc8e6787b8926cae51c605589b622058efb8c7fb
Alonso <abrahamreynoso64@gmail.com>
2024-03-02 23:09:11 -0600

    Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard'}, //TODO: Definir rutas hijas del dashboard
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {path: '**'} //TODO: Definir componente para el wildcard
+];
