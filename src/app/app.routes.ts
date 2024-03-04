28c93d49f5d863cdaad799695fd52c1fc6343ce6
anthony <tony20402014@gmail.com>
2024-03-03 19:11:29 -0600

    si

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import {WelcomeComponent} from "./Views/welcome/welcome.component";
 import { IndexComponent } from './Views/User/index/index.component';
 import {RegisterFormComponent} from "./Components/register-form/register-form.component";
+import {LoginFormComponent} from "./Components/login-form/login-form.component";
 
 export const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent,
@@ -10,6 +11,8 @@ export const routes: Routes = [
       {path: 'welcome', component: WelcomeComponent, title: 'Welcome'},
       {path: 'register', component: RegisterFormComponent, title: 'Register'},
       {path: 'users', component: IndexComponent, title: 'Users'},
+      {path: 'login', component: LoginFormComponent, title: 'login'},
+
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
     ]
   },
