a83ca488a93ac05b62991ad72addd5e463b94b6e
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-03-04 22:02:32 -0600

    iconos

---
+++ src/app/environments/environments.local.ts
@@ -0,0 +1,9 @@
+// src/environments/environment.local.ts
+export const environment = {
+  production: false,
+  index: 'http://127.0.0.1:8000/api/index',
+  store: 'http://127.0.0.1:8000/api/store',
+  login: 'http://127.0.0.1:8000/api/login',
+  delete: (userId: number) => `http://127.0.0.1:8000/api/delete/${userId}`
+
+};
